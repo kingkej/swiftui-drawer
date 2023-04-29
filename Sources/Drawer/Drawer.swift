e3c5405370f23ff2bc35e0395bf782e4f6021b8f
Kej <asd@sketchyzdesign.ru>
2023-04-29 09:38:21 +0500

    Add gesture dismiss

--- Sources/Drawer/Drawer.swift
+++ Sources/Drawer/Drawer.swift
@@ -4,93 +4,109 @@ import SwiftUI
 /// Generic Drawer container that slides in `Menu` from the left edge on top of a given `Content`.
 /// It occupies all proposed space and it recommended to be placed in the root of our scene in terms of layout size.
 public struct Drawer<Menu: View, Content: View>: View {
-
-  @Binding private var isOpened: Bool
-
-  private let menu: Menu
-  private let content: Content
-
-  // MARK: - Init
-
-  /// Instantiates new `Drawer` instance.
-  /// - Parameters:
-  ///   - isOpened: Binding to a bool that let's you control whether `Menu` is presented onscreen or not. Siblings can
-  ///   get access to it by adding `@Environment(\.isDrawerOpened) var isDrawerOpened` variable.
-  ///   - menu: `Menu` to be displayed on top of `Content` by sliding it from the left edge.
-  ///   - content: Content to be placed in the center of the screen. Occupies all given size.
-  public init(
-    isOpened: Binding<Bool>,
-    @ViewBuilder menu:  () -> Menu,
-    @ViewBuilder content: () -> Content
-  ) {
-    _isOpened = isOpened
-    self.menu = menu()
-    self.content = content()
-  }
-
-  // MARK: - Body
-
-  public var body: some View {
-    ZStack(alignment: .leading) {
-      content
-
-      if isOpened {
-        Color.clear
-          .contentShape(Rectangle())
-          .onTapGesture {
-            if isOpened {
-              isOpened.toggle()
-            }
-          }
-
-        menu
-          .transition(.move(edge: .leading))
-          .zIndex(1)
-      }
+    
+    @Binding private var isOpened: Bool
+    private let menuWidth: CGFloat = 300
+    @State private var menuOffset: CGFloat = 0
+    private let menu: Menu
+    private let content: Content
+    
+    // MARK: - Init
+    
+    /// Instantiates new `Drawer` instance.
+    /// - Parameters:
+    ///   - isOpened: Binding to a bool that let's you control whether `Menu` is presented onscreen or not. Siblings can
+    ///   get access to it by adding `@Environment(\.isDrawerOpened) var isDrawerOpened` variable.
+    ///   - menu: `Menu` to be displayed on top of `Content` by sliding it from the left edge.
+    ///   - content: Content to be placed in the center of the screen. Occupies all given size.
+    public init(
+        isOpened: Binding<Bool>,
+        @ViewBuilder menu:  () -> Menu,
+        @ViewBuilder content: () -> Content
+    ) {
+        _isOpened = isOpened
+        self.menu = menu()
+        self.content = content()
     }
-    .animation(.spring(), value: isOpened)
-    .environment(\.isDrawerOpened, $isOpened)
-  }
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        ZStack(alignment: .leading) {
+            content
+            
+            if isOpened {
+                Color.clear
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        if isOpened {
+                            isOpened.toggle()
+                        }
+                    }
+                
+                menu
+                    .transition(.move(edge: .leading))
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            let translation = value.translation.width
+                            if isOpened {
+                                menuOffset = max(0, translation)
+                            } else {
+                                menuOffset = min(0, translation + menuWidth)
+                            }
+                        }
+                        .onEnded { value in
+                            if isOpened {
+                                isOpened.toggle()
+                            }
+                        }
+                    )
+                    .zIndex(1)
+            }
+        }
+        .animation(.spring(), value: isOpened)
+        .environment(\.isDrawerOpened, $isOpened)
+    }
 }
 
 struct Drawer_Previews: PreviewProvider {
-
-  struct Preview: View {
-
-    @State private var isOpened = true
-    @State private var counter = 0
-
-    var body: some View {
-      Drawer(
-        isOpened: $isOpened,
-        menu: {
-          ZStack {
-            Color.gray
-
-            Button("Hide") {
-              isOpened = false
-            }
-          }
-          .frame(width: 100)
-        },
-        content: {
-          ZStack {
-            Color.yellow
-
-            Button("Open: \(counter)") {
-              withAnimation {
-                isOpened = true
-                counter += 1
-              }
-            }
-          }
-          .ignoresSafeArea()
+    
+    struct Preview: View {
+        
+        @State private var isOpened = true
+        @State private var counter = 0
+        
+        var body: some View {
+            Drawer(
+                isOpened: $isOpened,
+                menu: {
+                    ZStack {
+                        Color.gray
+                        
+                        Button("Hide") {
+                            isOpened = false
+                        }
+                    }
+                    .frame(width: 100)
+                },
+                content: {
+                    ZStack {
+                        Color.yellow
+                        
+                        Button("Open: \(counter)") {
+                            withAnimation {
+                                isOpened = true
+                                counter += 1
+                            }
+                        }
+                    }
+                    .ignoresSafeArea()
+                }
+            )
         }
-      )
     }
-  }
-
-  static var previews: some View {
-    Preview()
-  }
+    
+    static var previews: some View {
+        Preview()
+    }
 }
